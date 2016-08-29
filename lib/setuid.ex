85bb3d18fd30285592d2bf9766cfed600f47483e
Vladislav Petrov <electronicchest@gmail.com>
2016-08-29 22:21:20 +0300

    Fix path to maked lib

--- lib/setuid.ex
+++ lib/setuid.ex
@@ -1,7 +1,10 @@
 defmodule Setuid do
   @on_load :load_nifs
 
-  def load_nifs, do: :erlang.load_nif('./priv/setuid', 0)
+  def load_nifs do
+    path = :filename.join(:code.priv_dir(:setuid), 'setuid')
+    :erlang.load_nif(path, 0)
+  end
 
   def getuid,       do: raise "NIF getuid/0 not implemented"
   def setuid(_uid), do: raise "NIF setuid/1 not implemented"
