5c9249204b664642f81096a2b40825df6a165038
Vladislav Petrov <electronicchest@gmail.com>
2016-08-30 15:54:42 +0300

    Add Privilegex#change

--- lib/privilegex.ex
+++ lib/privilegex.ex
@@ -12,4 +12,15 @@ defmodule Privilegex do
   def setgid(_gid),          do: raise "NIF setgid/1 not implemented"
   def getpwnam(_user_name),  do: raise "NIF getpwnam/1 not implemented"
   def getgrnam(_group_name), do: raise "NIF getgrnam/1 not implemented"
+
+  def change(user_name), do: change(user_name, user_name)
+  def change(user_name, group_name) do
+    {:ok, %{uid: uid}} = getpwnam(user_name)
+    {:ok, %{gid: gid}} = getgrnam(group_name)
+
+    :ok = setuid uid
+    :ok = setgid gid
+
+    :ok
+  end
 end
--- test/privilegex_test.exs
+++ test/privilegex_test.exs
@@ -2,6 +2,13 @@ defmodule PrivilegexTest do
   use ExUnit.Case
   doctest Privilegex
 
+  setup do
+    user_name = String.strip(elem(System.cmd("whoami", []), 0))
+    group_name = String.strip(elem(System.cmd("id", ["-gn"]), 0))
+
+    {:ok, %{user_name: user_name, group_name: group_name}}
+  end
+
   test "#getuid" do
     assert {:ok, _} = Privilegex.getuid
   end
@@ -10,15 +17,15 @@ defmodule PrivilegexTest do
     assert {:ok, _} = Privilegex.getgid
   end
 
-  test "#getpwnam" do
-    user_name = String.strip(elem(System.cmd("whoami", []), 0))
-
+  test "#getpwnam", %{user_name: user_name} do
     assert {:ok, %{gecos: _, gid: _, name: _, passwd: _, uid: _}} = Privilegex.getpwnam(user_name)
   end
 
-  test "#getgrnam" do
-    group_name = String.strip(elem(System.cmd("id", ["-gn"]), 0))
-
+  test "#getgrnam", %{group_name: group_name} do
     assert {:ok, %{gid: _, name: _, passwd: _}} = Privilegex.getgrnam(group_name)
+  end
+
+  test "#change", %{user_name: user_name, group_name: group_name} do
+    assert :ok = Privilegex.change(user_name, group_name)
   end
 end
